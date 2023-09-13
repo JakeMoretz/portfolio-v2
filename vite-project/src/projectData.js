5e8004cb9521d0a4fc974824188fc11004cb6910
Jake Moretz <jmoretz9090@gmail.com>
2023-09-12 21:32:52 -0400

    added new project to project card

--- vite-project/src/projectData.js
+++ vite-project/src/projectData.js
@@ -10,6 +10,15 @@ export default [
     },
     {
         id: '2',
+        title: 'Task List App',
+        description: 'Task List is a simple web application that helps you manage your tasks. You can add, complete, delete, and reset tasks easily using Firebase Firestore for data storage.',
+        stack: 'React.js, CSS, Firebase',
+        img: './task-list.png',
+        live: 'https://jakemoretz.github.io/task-list/',
+        code: 'https://github.com/JakeMoretz/task-list',
+    },
+    {
+        id: '3',
         title: 'Mini Weather App',
         description: 'Here we have a mini weather app, fetching data from a Weather API. We can now display current temp, humidity, and wind mph with a single button click.',
         stack: 'HTML, CSS, Javascript',
