20a4bbe96c95a16958fd12376bec019f3028e8d5
Jake Moretz <jmoretz9090@gmail.com>
2024-01-16 12:38:55 -0500

    added movie watchlist project to portfolio

--- vite-project/src/projectData.js
+++ vite-project/src/projectData.js
@@ -1,6 +1,15 @@
 export default [
     {
         id: '1',
+        title: 'Movie Watchlist',
+        description: "This app is a movie watchlist tool that utilizes an API to search for movies. Users can easily add films to their personalized watchlist and have the flexibility to remove them once they've been watched.",
+        stack: 'Javascript, CSS, HTML, OMDb API',
+        img: './watchlist.png',
+        live: 'https://jakemoretz.github.io/movie-watchlist/',
+        code: 'https://github.com/JakeMoretz/movie-watchlist',
+    },
+    {
+        id: '2',
         title: 'Portfolio Project',
         description: "This very portfolio you're viewing is also a project I've developed. It's designed to be customizable and easy to use.",
         stack: 'React.js, CSS',
@@ -9,7 +18,7 @@ export default [
         code: 'https://github.com/JakeMoretz/portfolio-v2/tree/main/vite-project',
     },
     {
-        id: '2',
+        id: '3',
         title: 'Task List App',
         description: 'Task List is a simple web application that helps you manage your tasks. You can add, complete, delete, and reset tasks easily using Firebase Firestore for data storage.',
         stack: 'React.js, CSS, Firebase',
